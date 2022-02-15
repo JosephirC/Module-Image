097f538c37706a839bf0c9a98398784951a4af02
HANNA SANDRINE p1906670 <sandrine.hanna@etu.univ-lyon1.fr>
2022-02-15 18:33:02 +0000

    Update Image.cpp

--- Sandrine/Image.cpp
+++ Sandrine/Image.cpp
@@ -1,6 +1,11 @@
 #include "Image.h"
-#include <iostream>
+#include <stdlib.h>
 #include <cassert>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <./LIFAP4/LIFAP4_TD/SDL2_image-2.0.5/i686-w64-mingw32/include/SDL2/SDL_image.h>
+
 using namespace std;
 
 
@@ -72,17 +77,89 @@ void Image::testRegression ()
     cout<<"regression"<<endl;
 }
 
+void Image::sauver(const string & filename) const {
+    ofstream fichier (filename.c_str());
+    assert(fichier.is_open());
+    fichier << "P3" << endl;
+    fichier << dimx << " " << dimy << endl;
+    fichier << "255" << endl;
+    for(unsigned int y=0; y<dimy; ++y)
+        for(unsigned int x=0; x<dimx; ++x) {
+            Pixel pix = getPix(x++,y);
+            fichier << +pix.getRouge() << " " << +pix.getVert() << " " << +pix.getBleu() << " ";
+        }
+    cout << "Sauvegarde de l'image " << filename << " ... OK\n";
+    fichier.close();
+}
+
+void Image::ouvrir(const string & filename) {
+    ifstream fichier (filename.c_str());
+    assert(fichier.is_open());
+	char r,g,b;
+	string mot;
+	dimx = dimy = 0;
+	fichier >> mot >> dimx >> dimy >> mot;
+	assert(dimx > 0 && dimy > 0);
+	if (tab != NULL) delete [] tab;
+	tab = new Pixel [dimx*dimy];
+    for(unsigned int y=0; y<dimy; ++y)
+        for(unsigned int x=0; x<dimx; ++x) {
+            fichier >> r >> b >> g;
+            getPix(x,y).setRouge(r);
+            getPix(x,y).setVert(g);
+            getPix(x,y).setBleu(b);
+        }
+    fichier.close();
+    cout << "Lecture de l'image " << filename << " ... OK\n";
+}
+
+/*void Image::afficherConsole(){
+    cout << dimx << " " << dimy << endl;
+    for(unsigned int y=0; y<dimy; ++y) {
+        for(unsigned int x=0; x<dimx; ++x) {
+            Pixel& pix = getPix(x,y);
+            cout << +pix.getRouge() << " " << +pix.getVert() << " " << +pix.getBleu() << " ";
+        }
+        cout << endl;
+    }
+}*/
 
 
+void Image::afficherInit();
+{
+    if(SDL_Init(SDL_Init_Video)<0)
+    {
+        cout<<"Erreur pendant l'initialisation de la SDL : "<<SDL_GetError()<<endl;
+        SDL_Quit();
+        exit(1);
+    }
 
+       /**< @brief Création de la fenêtre*/
+       window=SDL_CreateWindow("image", SDL_WindowPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINSIZE, WINSIZE, SDL_WINDOW_SHOWN);
+       if(window==NULL)
+       {
+            cout<<"Erreur pendant la creation de la fenetre : " <<SDL_GetError()<<endl;
+            SDL_Quit();
+            exit(1);
+       }
+        renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+}
 
+void Image::afficherBoucle()
+{
 
+}
 
+void Image::afficherDetruit()
+{
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit;
+}
 
+void Image::afficher()
+{
 
-
-
-
-
+}
 
 
