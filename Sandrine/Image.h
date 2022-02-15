12d946af7312aac6d2ddccf0ab4f715aff90d972
HANNA SANDRINE p1906670 <sandrine.hanna@etu.univ-lyon1.fr>
2022-02-15 18:33:28 +0000

    Update Image.h

--- Sandrine/Image.h
+++ Sandrine/Image.h
@@ -7,39 +7,39 @@ class Image{
 
  private :
 
-    Pixel *tab;     // le tableau 1D de pixel
-    int dimx, dimy;        // les dimensions de l'image
+    Pixel *tab;             /**< @param   le tableau 1D de pixel*/
+    int dimx, dimy;       /**< @param  les dimensions de l'image*/
 
  public :
 
-   // Constructeur par défaut de la classe: initialise dimx et dimy à 0
-   // ce constructeur n'alloue pas de pixel
+   /**< @brief  Constructeur par défaut de la classe: initialise dimx et dimy Ã  0
+    ce constructeur n'alloue pas de pixel */
     Image();
 
-   // Constructeur de la classe: initialise dimx et dimy (après vérification)
-   // puis alloue le tableau de pixel dans le tas (image noire)
+   /**< @brief  Constructeur de la classe: initialise dimx et dimy (aprÃ¨s vÃ©rification)
+    puis alloue le tableau de pixel dans le tas (image noire)*/
     Image(int dimensionX, int dimensionY);
 
-   // Destructeur de la classe: déallocation de la mémoire du tableau de pixels
-   // et mise à jour des champs dimx et dimy à 0
+   /**< @brief  Destructeur de la classe: dÃ©allocation de la mÃ©moire du tableau de pixels
+    et mise Ã  jour des champs dimx et dimy Ã  0*/
     ~Image();
 
-   // Accesseur : récupère le pixel original de coordonnées (x,y) en vérifiant leur validité
-   // la formule pour passer d'un tab 2D à un tab 1D est tab[y*dimx+x]
+   /**< @brief  Accesseur : récupÃ¨re le pixel original de coordonnÃ©es (x,y) en vÃ©rifiant leur validitÃ©
+    la formule pour passer d'un tab 2D Ã  un tab 1D est tab[y*dimx+x]*/
     Pixel getPix (int x, int y) const; //'Pixel' (l'original, pas une copie)
 
-   // Mutateur : modifie le pixel de coordonnées (x,y)
+   /**< @brief   Mutateur : modifie le pixel de coordonnÃ©es (x,y)*/
     void setPix (int x, int y, Pixel couleur);
 
-   // Dessine un rectangle plein de la couleur dans l'image (en utilisant setPix, indices en paramètre compris)
+   /**< @brief  Dessine un rectangle plein de la couleur dans l'image (en utilisant setPix, indices en paramÃ¨tre compris)*/
     void dessinerRectangle (int Xmin, int Ymin, int Xmax, int Ymax, Pixel couleur);
 
-   // Efface l'image en la remplissant de la couleur en paramètre
-   // (en appelant dessinerRectangle avec le bon rectangle)
+   /**< @brief  Efface l'image en la remplissant de la couleur en paramÃ¨tre
+    (en appelant dessinerRectangle avec le bon rectangle)*/
     void effacer (Pixel couleur);
 
-   // Effectue une série de tests vérifiant que le module fonctionne et
-   // que les données membres de l'objet sont conformes
+   /**< @brief  Effectue une sÃ©rie de tests vÃ©rifiant que le module fonctionne et
+    que les donnÃ©es membres de l'objet sont conformes*/
     void testRegression ();
 
     void sauver(const string & filename) const;
@@ -47,6 +47,8 @@ class Image{
     void ouvrir(const string & filename);
 
     void afficherConsole();
+
+    void afficher ();
 
 };
 
