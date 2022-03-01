b3eeebc07a6c340de879635bab86616896fa59e3
ABIDA YOUSSEF p2024398 <youssef.abida@etu.univ-lyon1.fr>
2022-03-01 08:56:40 +0000

    Pixel.h avec la documentation doxygen

---
+++ Josephir/Pixel.h
@@ -0,0 +1,38 @@
+#ifndef _PIXEL_H
+#define _PIXEL_H
+
+class Pixel{
+
+    private :
+                unsigned int r,g,b; 
+                /**< @param les composantes du pixel, unsigned char en C++*/
+                
+    public :
+
+                /**< @brief  Constructeur par défaut de la classe: initialise le pixel à  la couleur noire*/                
+                Pixel ();
+
+                /**< @brief  Constructeur de la classe: initialise r,g,b avec les paramètres*/
+                Pixel (int nr, int ng, int nb);
+
+                /**< @brief  Accesseur : récupère la composante rouge du pixel*/
+                int getRouge ();
+
+                /**< @brief  Accesseur : récupère la composante verte du pixel*/
+                int getVert ();
+
+                /**< @brief  Accesseur : récupère la composante bleue du pixel*/
+                int getBleu ();
+
+                /**< @brief  Mutateur : modifie la composante rouge du pixel*/
+                void setRouge (int nr);
+
+                /**< @brief  Mutateur : modifie la composante verte du pixel*/
+                void setVert (int ng);
+
+                /**< @brief  Mutateur : modifie la composante bleue du pixel*/
+                void setBleu (int nb);
+
+};
+
+#endif
