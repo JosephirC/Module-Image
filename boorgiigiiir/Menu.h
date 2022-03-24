bcf945e123a1b888000970842435ca16830aac68
ABIDA YOUSSEF p2024398 <youssef.abida@etu.univ-lyon1.fr>
2022-03-24 12:50:39 +0000

    Upload New File

---
+++ boorgiigiiir/Menu.h
@@ -0,0 +1,27 @@
+#ifndef _MENU_H
+#define _MENU_H
+
+
+#include <string>
+#include <iostream>
+
+using namespace  std;
+
+class Menu{
+
+    private :
+  
+    int niv;
+
+    public :
+
+        Menu();
+
+        ~Menu();
+
+
+        // deux fct affichage menu solo / mult 
+        
+};
+
+#endif
